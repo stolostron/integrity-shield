a2a3d2de20978e8fcfcf4c7117a8d8b732c12692
ruriko <rurikudo@ibm.com>
2021-10-06 19:19:09 +0900

    fix copyright check script
    Signed-off-by: ruriko <rurikudo@ibm.com>

--- build/copyright-check.sh
+++ build/copyright-check.sh
@@ -31,7 +31,6 @@ function fail()
 
 # Enforce check for copyright statements in Go code
 GOSRCFILES=($(find ./ -type f -name \*.go -or -name \*.sh -or -name Dockerfile))
-THISYEAR=$(date +"%Y")
 for GOFILE in "${GOSRCFILES[@]}"; do
   if ! grep -q "Licensed under the Apache License, Version 2.0" $GOFILE; then
     fail "Missing copyright/licence statement in ${GOFILE}"
@@ -39,27 +38,6 @@ for GOFILE in "${GOSRCFILES[@]}"; do
   fi
 done 
 
-
-# GOSRCFILES=($(find ./ -type f -name \*.go -or -name \*.sh -or -name Dockerfile))
-# for GOFILE in "${GOSRCFILES[@]}"; do
-# if grep -q "Copyright .* IBM Corporation" $GOFILE; then
-#     YEAR_LINE=$(grep "Copyright .* IBM Corporation" $GOFILE)
-#     YEARS=($(echo $YEAR_LINE | grep -oE '[0-9]{4}'))
-#     if [[ ${#YEARS[@]} == 1 ]]; then
-#         if [[ ${YEARS[0]} != ${THISYEAR} ]]; then
-#             fail "Single out-of-date copyright in ${GOFILE}."
-#             CRC=$(($CRC + 1))
-#         fi
-#     elif [[ ${#YEARS[@]} == 2 ]]; then
-#         if [[ ${YEARS[1]} != ${THISYEAR} ]]; then
-#             fail "Double year copyright with out-of-date second year in ${GOFILE}."
-#             CRC=$(($CRC + 1))
-#         fi
-#     else
-#         echo "#YEARS was ${#YEARS[@]} in $GOFILE"
-#     fi
-# fi
-# done
-if [ $CRC -gt 0 ]; then fail "Please run make copyright to add copyright statements and check in the updated file(s).\n"; fi
+if [ $CRC -gt 0 ]; then fail "Please add copyright statements and check in the updated file(s).\n"; fi
 
 exit $CRC
